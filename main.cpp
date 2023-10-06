c583ee409034e9f5e4ad8b68b7c41f6bd90411c7
AndyLu <laserlue@gmail.com>
2023-10-06 13:34:02 -0600

    feat: read multiple files
    Read multiple files under the same folder.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "reader.h"
 
+#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -8,8 +9,14 @@ using namespace std;
 int main(void) {
   Reader *r = new Reader();
   try {
-    string s = r->readText("./data/test.txt");
-    cout << s << endl;
+    const string filepath = "./data";
+    for (const auto &entry : filesystem::directory_iterator(filepath)) {
+      if (entry.path().extension() == ".txt") {
+        cout << "File name: " << entry.path() << endl;
+        string s = r->readText(entry.path());
+        cout << s << endl;
+      }
+    }
   } catch (std::invalid_argument &e) {
     cout << e.what() << endl;
     return -1;
